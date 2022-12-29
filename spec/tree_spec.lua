534f853fa9bbd497cc556ec1844829a10a25b832
xrowii <wangrongfei0218@outlook.com>
2022-12-30 00:56:17 +0800

    Add test for `Tree.get_changed_ranges`

--- spec/tree_spec.lua
+++ spec/tree_spec.lua
@@ -20,4 +20,22 @@ describe("Tree", function()
 			"ltreesitter.Node"
 		)
 	end)
+	it("get_changed_ranges should return changed ranges", function()
+		t:edit_s {
+			start_byte    = 18,
+			old_end_byte  = 18,
+			new_end_byte  = 25,
+			start_point   = { row = 0, column = 18 },
+			old_end_point = { row = 0, column = 18 },
+			new_end_point = { row = 0, column = 25 },
+		}
+		local u = p:parse_string([[ int main(void) { int a; return 0; } ]], t)
+		local c = t:get_changed_ranges(u)
+		assert.are.same({{
+			start_byte  = 18,
+			end_byte    = 24,
+			start_point = { row = 0, column = 18 },
+			end_point   = { row = 0, column = 24 },
+		}}, c)
+	end)
 end)
