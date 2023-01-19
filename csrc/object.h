33d91592b470072853438366d39cff4efa0ce416
Corey Williamson <euclidianAce@protonmail.com>
2023-01-19 01:33:25 -0600

    object.h should include lua.h

--- csrc/object.h
+++ csrc/object.h
@@ -1,6 +1,8 @@
 #ifndef LTREESITTER_OBJECT_H
 #define LTREESITTER_OBJECT_H
 
+#include <lua.h>
+
 void setup_object_table(lua_State *L);
 void push_parent(lua_State *L, int obj_idx);
 void set_parent(lua_State *L, int parent_idx);
