da100e7a9b2589f3ffda4a866dda66f9b9479a33
takase1121 <20792268+takase1121@users.noreply.github.com>
2023-01-23 21:32:46 +0800

    module: set visibility attribute on non-Windows platform

--- include/ltreesitter/module.h
+++ include/ltreesitter/module.h
@@ -6,7 +6,7 @@
 #ifdef _WIN32
 #define LTREESITTER_EXPORT __declspec (dllexport)
 #else
-#define LTREESITTER_EXPORT
+#define LTREESITTER_EXPORT __attribute__ ((visibility("default")))
 #endif
 
 LTREESITTER_EXPORT int luaopen_ltreesitter(lua_State *L);
