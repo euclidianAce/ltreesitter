c1856d822039f8d02e398ecce6c87bfd1ab156d2
Corey Williamson <euclidianAce@protonmail.com>
2025-08-14 19:20:13 -0500

    Update version string
    Oops

--- csrc/ltreesitter.c
+++ csrc/ltreesitter.c
@@ -13,7 +13,7 @@
 #include <ltreesitter/tree_cursor.h>
 
 // @teal-export version: string
-static const char version_str[] = "0.0.7";
+static const char version_str[] = "0.1.0+dev";
 
 static const luaL_Reg lib_funcs[] = {
 	{"load", ltreesitter_load_parser},
